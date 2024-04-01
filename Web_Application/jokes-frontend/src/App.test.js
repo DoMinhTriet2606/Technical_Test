ab759288e6177095fabf2095cc573452ae437ea6
Minh Triet <nightcorekz69@gmail.com>
2024-04-01 12:59:30 +0700

    ci: add script frontend CI

--- Web_Application/jokes-frontend/src/App.test.js
+++ Web_Application/jokes-frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import Footer from "./Footer";
-import Main from "./Main";
-import Header from "./Header";
-import Slider from "./Slider";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
 
 describe("Footer Component", () => {
     test("renders disclaimer text", () => {
